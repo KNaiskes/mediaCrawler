82725c7f0e5a2047ec468703d9bd1811963a10f9
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-01-26 01:44:19 +0200

    url results are saved in lists instead of files and urls are displayed in a page

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template,redirect,request,url_for,session
 from db.database import *
+import os.path
+#from os import makedirs
+#from os import chdir
+from crawlFunctions import *
 
 app = Flask(__name__)
 app.secret_key = "keep_it_secret_keep_it_safe"
+
+keyword = ""
 
 @app.route("/")
 def index():
@@ -17,15 +23,31 @@ def login():
 		else:
 			session["logged_in"] = True
 			session["username"] = request.form["username"]
-			return redirect(url_for("dashboard"))
+			#if not os.path.exists("results/"+session["username"]):
+					#makedirs("results/"+session["username"])
+			return redirect(url_for("search"))
 	return render_template("login.html", error = error)
 
-				
-@app.route("/dashboard")
-def dashboard():
+@app.route("/search", methods=["GET", "POST"])
+def search():
 	if not session.get("logged_in"):
 		return redirect(url_for("login"))
-	return render_template("dashboard.html")
+	global keyword
+	keyword = request.form.get("keyword")
+	print("Here is the keyword:", keyword)
+	return render_template("search.html")
+				
+@app.route("/results")
+def results():
+	if not session.get("logged_in"):
+		return redirect(url_for("login"))
+	#chdir("results/"+session["username"])
+	fb = fbPlaces(keyword)
+	tweets = getTweets(keyword)
+	flickr = getFlickr(keyword)
+	gplus = getGplus(keyword)
+	reddit = getReddit(keyword)
+	return render_template("results.html",fb=fb)
 
 
 if __name__ == "__main__":
