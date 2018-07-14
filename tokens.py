9f790ac99888419a90753ffbd3e225e784e0f707
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-07-14 21:24:27 +0300

    changed tokens from every service in order to make project public

--- tokens.py
+++ tokens.py
@@ -1,25 +1,25 @@
 #Facebook credentials
-FacebookToken = """EAAfDj8Qa6o8BADFdXDN2ZCv9o9lDq9qEgIad4sOPqoZB939Ru5VVCaEe294iedl19ZAcIn3M31X2UnZBZAiEbaeQbBvZA4tFNlRQNsL8nfPff51FxIoOXLUdJwH6utMjyFRaIxqLqTu2ZBBLJCIHIH2f9kUKcrTmZAEfY0l71HWk15B5DdiSZBfzX"""
+FacebookToken = """"""
 
 #Twitter credentials
-Twitter_consumer_key = "MDW02nMPJrjzOYyGAEqbGShFP"
-Twitter_consumer_secret = "bu1m3eVgX5cPAiTu6KcranLeJ6NPdX93zA2qNhqkFbeNmjAHeZ" 
-Twitter_access_token = "4816940682-NW4LejaGeoKMUe2thHrYKWoiAvWSh9DqmFvsqxJ"
-Twitter_access_token_secret = "RLOfODNPnM0AuSrR6XGnIxhdM49NYR67PAKtqIe0TzvrA" 
+Twitter_consumer_key = ""
+Twitter_consumer_secret = "" 
+Twitter_access_token = ""
+Twitter_access_token_secret = "" 
 
 
 #Flickr
-Flickr_key = "b4e913643345419ed0c9485ead79ffcb"
-Flickr_secret = "9b887f70c500540e"
+Flickr_key = ""
+Flickr_secret = ""
 
 #G+
-GooglePlus_key = "AIzaSyCXru8PkSK1ItS5sMMJSQNBh_b6epzmsbE"
+GooglePlus_key = ""
 
 #Reddit
 
-Reddit_client_id = "kJRrYyBiQgIXmg"
-Reddit_client_secret = "z8HqW-DEz0E-avV9rWCPYaLA4Sc"
-Reddit_username = "knBot"
-Reddit_password = "search4loop"
-Reddit_userAgent = "hello_internet"
+Reddit_client_id = ""
+Reddit_client_secret = ""
+Reddit_username = ""
+Reddit_password = ""
+Reddit_userAgent = ""
 
